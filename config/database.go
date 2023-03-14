c15190c4e823a3409070db14f9650fcfa4086829
Darrel <anselmusdarrel@gmail.com>
2023-03-14 20:45:07 +0700

    Upload All File

---
+++ config/database.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+
+	dsn := "root:@tcp(localhost:3306)/db_tes"
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
+	if err != nil {
+		panic("Cannot connect database")
+	}
+	DB.AutoMigrate()
+}
